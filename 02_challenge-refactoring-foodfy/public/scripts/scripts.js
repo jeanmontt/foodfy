4fb1eec54a36683f360e0211ac3d68a93a6aa338
Jean Monteiro <jeanmontt@hotmail.com>
2020-12-21 21:24:22 +0000

    Finished challenge refactoring foodfy

--- 02_challenge-refactoring-foodfy/public/scripts/scripts.js
+++ 02_challenge-refactoring-foodfy/public/scripts/scripts.js
@@ -1,12 +1,25 @@
-const modalOverlay = document.querySelector('.modal_overlay');
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
-    card.addEventListener("click", () => {
-        modalOverlay.classList.add('active');
-    })
+  card.addEventListener("click", () => {
+    recipeId = card.getAttribute('id');
+
+    window.location.href = `/receitas/${recipeId}`;
+  });
 }
 
-modalOverlay.querySelector('a').addEventListener("click", () => {
-    modalOverlay.classList.remove('active');
-})
+const content = document.querySelector('#content');
+const buttons = content.querySelectorAll('a');
+const lists = content.querySelectorAll('.list');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => {
+    if (lists[i].classList.contains("hidden")) {
+      lists[i].classList.remove("hidden");
+      buttons[i].innerHTML = "ESCONDER";
+    } else {
+      lists[i].classList.add("hidden");
+      buttons[i].innerHTML = "MOSTRAR";
+    }
+  });
+}

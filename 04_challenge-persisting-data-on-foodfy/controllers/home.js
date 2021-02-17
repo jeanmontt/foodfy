b5473766f1efe99d950e79c2ed9d6145116b2a1e
Jean Monteiro <jeanmontt@hotmail.com>
2021-02-17 16:54:08 +0000

    Initial commit persisting data on foodfy

---
+++ 04_challenge-persisting-data-on-foodfy/controllers/home.js
@@ -0,0 +1,26 @@
+const data = require("../data.json");
+
+exports.index = (req, res) => {
+  res.render("home/index", { data: data.recipes });
+}
+
+exports.about = (req, res) => {
+  res.render("home/about");
+}
+
+exports.recipes =(req, res) => {
+  res.render("home/recipes", { data: data.recipes });
+}
+
+exports.recipe = (req, res) => {
+  const recipeIndex = req.params.id;
+  const recipe = data.recipes.find((recipe) => {
+    return recipe.id == recipeIndex;
+  });
+
+  if (!recipe) {
+    return res.send("Receita não encontrada!")
+  }
+
+  res.render("home/recipe", { data: recipe });
+}
---
+++ 04_challenge-persisting-data-on-foodfy/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const home = require('./controllers/home');
+const admin = require('./controllers/admin')
+const routes = express.Router();
+
+routes.get("/", home.index);
+routes.get("/sobre", home.about);
+routes.get("/receitas", home.recipes);
+routes.get("/receitas/:id", home.recipe);
+
+routes.get("/admin/receitas", admin.index);
+routes.get("/admin/receitas/nova", admin.create);
+routes.get("/admin/receitas/:id", admin.show);
+routes.get("/admin/receitas/:id/editar", admin.edit);
+routes.post("/admin/receitas", admin.post);
+routes.put("/admin/receitas", admin.put); 
+routes.delete("/admin/receitas", admin.delete);
+module.exports = routes;
